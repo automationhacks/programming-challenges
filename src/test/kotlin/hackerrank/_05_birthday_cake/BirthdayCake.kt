3beac50f497d551c42f0f1a41d4533f878f58ffd
Gaurav Singh <gaurav.singh55@gmail.com>
2020-04-10 09:05:06 +0530

    Remove unused imports in BirthdayCake.kt

--- src/test/kotlin/hackerrank/_05_birthday_cake/BirthdayCake.kt
+++ src/test/kotlin/hackerrank/_05_birthday_cake/BirthdayCake.kt
@@ -1,23 +1,7 @@
 package hackerrank._05_birthday_cake
 
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
 import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
 import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
 
 fun birthdayCakeCandles(ar: Array<Int>): Int? {
     val candleHeightMapping = hashMapOf<Int, Int>()
@@ -26,11 +10,11 @@ fun birthdayCakeCandles(ar: Array<Int>): Int? {
         incrementMapValue(candleHeightMapping, candleHeight)
     }
 
-    val max : Int = candleHeightMapping.keys.max() ?: 0
+    val max: Int = candleHeightMapping.keys.max() ?: 0
     return candleHeightMapping[max]
 }
 
-fun incrementMapValue(aMap : HashMap<Int, Int>, key : Int) : HashMap<Int, Int> {
+fun incrementMapValue(aMap: HashMap<Int, Int>, key: Int): HashMap<Int, Int> {
     val value = aMap.getOrDefault(key, 0)
     aMap[key] = value + 1
     return aMap
@@ -38,7 +22,7 @@ fun incrementMapValue(aMap : HashMap<Int, Int>, key : Int) : HashMap<Int, Int> {
 
 fun main() {
     val scan = Scanner(System.`in`)
-    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+    val ar = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
     val result = birthdayCakeCandles(ar)
 
     println(result)
