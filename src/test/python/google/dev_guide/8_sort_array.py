36bd62b6ca41d830fa6759c00249c9529385a541
Gaurav Singh <gaurav.singh55@gmail.com>
2020-06-09 09:31:13 +0530

    Added solution to sort array with removing duplicates

---
+++ src/test/python/google/dev_guide/8_sort_array.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+
+
+def sort_with_dup_removed(arr):
+    if len(arr) <= 1:
+        return arr
+
+    dct = OrderedDict()
+    for num in arr:
+        if num not in dct:
+            dct[num] = 0
+
+    arr_with_dup_removed = list(dct.keys())
+    arr_with_dup_removed.sort()
+    return arr_with_dup_removed
+
+
+"""
+sort([]) → []
+sort([1]) → [1]
+sort([1, 1]) → [1]
+sort([1, 2, 10, 9, 9, 7, 8, 5]) → [1, 2, 5, 7, 8, 9, 10]
+
+"""
+
+
+def test_sort_with_less_than_1_item():
+    assert sort_with_dup_removed([1]) == [1]
+
+
+def test_sort_with_multiple_duplicates():
+    assert sort_with_dup_removed([1, 2, 10, 9, 9, 7, 8, 5]) == [1, 2, 5, 7, 8, 9, 10]
