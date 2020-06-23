41a6a4d612c3d6e182886e37068e5db07be3496c
Gaurav Singh <gaurav.singh55@gmail.com>
2020-06-23 09:13:48 +0530

    Added linked list impl (python from CTCI book solutions). Added solutions for remove dups and kth to last problems on singly linked list

---
+++ src/test/python/cracking_the_coding_interview/ch_02_linked_list/1_remove_dups.py
@@ -0,0 +1,63 @@
+"""
+**2.1 Remove Dups:** Write code to remove duplicates from an unsorted linked list.
+FOLLOW UP
+How would you solve this problem if a temporary buffer is not allowed? Hints: #9, #40
+
+Hints:
+
+- Have you tried a hash table? You should be able to do this in a single pass of the linked list.
+- Without extra space, you'll need a(N2) time. Try using two pointers, where the second
+one searches ahead of the first one.
+
+[Official solution](https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter2/1_Remove_Dups.py)
+"""
+from python.cracking_the_coding_interview.ch_02_linked_list.linked_list import LinkedList
+
+
+def remove_dups(ll):
+    if ll.head is None:
+        return
+
+    current = ll.head
+    seen = {current.value}
+
+    while current.next is not None:
+        if current.next.value in seen:
+            current.next = current.next.next
+        else:
+            seen.add(current.next.value)
+            current = current.next
+
+    return ll
+
+
+def remove_dups_follow_up(ll):
+    if ll.head is None:
+        return
+
+    current = ll.head
+    while current:
+        runner = current
+        while runner.next:
+            if runner.next.value == current.value:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        current = current.next
+
+    return ll
+
+
+def test_remove_dups():
+    ll = LinkedList()
+    ll.generate(20, 0, 9)
+    print()
+    print(f'Before removing dups {ll}')
+    remove_dups(ll)
+    print(f'After removing dups {ll}')
+
+    ll.generate(20, 0, 9)
+    print()
+    print(f'Before removing dups {ll}')
+    remove_dups_follow_up(ll)
+    print(f'After removing dups {ll}')
---
+++ src/test/python/cracking_the_coding_interview/ch_02_linked_list/2_kth_to_last.py
@@ -0,0 +1,78 @@
+"""
+**2.2 Kth to last** Implement an algorithm to find the kth to last element of a singly linked list.
+
+Hints: #8, #25, #47, #67, # 726
+
+- What if you knew the linked list size? What is the difference between finding the Kth-to- last element and finding the Xth element?
+- If you don't know the linked list size, can you compute it? How does this impact the runtime?
+- Look at this graph. Is there any node you can identify that will definitely be okay to build first?
+- You might find it useful to return multiple values. Some languages don't directly support this, but there are workarounds in essentially any language. What are some of those workarounds?
+- We're probably going to run this algorithm many times. If we did more preprocessing, is there a way we could optimize this?
+
+[Official solution](https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter2/2_Return_Kth_To_Last.py)
+"""
+from python.cracking_the_coding_interview.ch_02_linked_list.linked_list import LinkedList
+
+
+def kth_to_last(ll, k):
+    if ll.head is None:
+        return
+
+    length = 0
+    current = ll.head
+    while current:
+        length += 1
+        current = current.next
+
+    stop = length - k
+    index = 0
+
+    current = ll.head
+    while current:
+        if index == stop:
+            return current.value
+        else:
+            current = current.next
+            index += 1
+
+    return None
+
+
+def kth_to_last_runner(ll, k):
+    """
+    Runner technique:
+    current: points to head
+    runner: runs till kth position
+    Once reached, both current and runner move till runner reaches the end
+    We then can return the data at the current's position
+    then runner is increamen
+    :param ll:
+    :param k:
+    :return:
+    """
+    if ll.head is None:
+        return
+
+    runner = current = ll.head
+    for i in range(k):
+        if runner is None:
+            return None
+        runner = runner.next
+
+    while runner:
+        current = current.next
+        runner = runner.next
+
+    return current.value
+
+
+def test_kth_to_last():
+    ll = LinkedList()
+    ll.generate(6, 0, 99)
+    k = 3
+    print()
+    print(f'Given LL: {ll} and k: {k}')
+    value = kth_to_last(ll, k)
+    print(value)
+    value = kth_to_last_runner(ll, k)
+    print(value)
