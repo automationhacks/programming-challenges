423eaef49a42aee26774f1ea59ab61428c90b6b1
Gaurav Singh <gaurav.singh55@gmail.com>
2020-06-25 22:02:14 +0530

    Solution: Chapter 2. Problem 2.5 Solution to follow up question

--- src/test/python/cracking_the_coding_interview/ch_02_linked_list/5_sum_lists.py
+++ src/test/python/cracking_the_coding_interview/ch_02_linked_list/5_sum_lists.py
@@ -20,6 +20,18 @@ Output: 9 => 1 => 2.
 That is, 912.
 
 Hints: #7, #30, #71, #95, #109
+
+- Of course, you could convert the linked lists to integers, compute the sum, and then convert it back to a new linked
+list. If you did this in an interview, your interviewer would likely accept the answer, and then see if you could do
+this without converting it to a number and back.
+- Try recursion. Suppose you have two lists, A = 1 - >5 - >9 (representing 951) and B 2- >3- >6- >7 (representing 7632)
+, and a function that operates on the remainder of the lists (5 - >9 and 3- >6 - >7). Could you use this to create
+the sum method? What is the relationship between sum(1->5->9, 2->3->6->7) and sum(5->9, 3->6->7)?
+- Make sure you have considered linked lists that are not the same length.
+- Does your algorithm work on linked lists like 9->7->8 and 6->8->5? Double check that.
+- For the follow-up question:The issue is that when the linked lists aren't the same length, the head of one linked
+list might represent the 1000's place while the other represents the 1D's place. What if you made them the same
+length? Is there a way to modify the linked list to do that, without changing the value it represents?
 """
 from python.cracking_the_coding_interview.ch_02_linked_list.linked_list import LinkedList
 
@@ -42,6 +54,26 @@ def sum_list(first_ll, second_ll):
     return sum_ll
 
 
+def sum_list_follow_up(first_ll, second_ll):
+    if len(first_ll) < len(second_ll):
+        for i in range(len(second_ll) - len(first_ll)):
+            first_ll.add_to_beginning(0)
+    else:
+        for i in range(len(first_ll) - len(second_ll)):
+            second_ll.add_to_beginning(0)
+
+    n1, n2 = first_ll.head, second_ll.head
+    result = 0
+    while n1 or n2:
+        result = (result * 10) + n1.value + n2.value
+        n1 = n1.next
+        n2 = n2.next
+
+    ll = LinkedList()
+    ll.add_multiple([int(i) for i in str(result)])
+    return ll
+
+
 def add_nos_with_carry(carry, first, second):
     first_no = first.value
     second_no = second.value
@@ -58,5 +90,6 @@ def test_sum_list():
     second = LinkedList()
     second.add_multiple([5, 9, 2])
 
-    total = sum_list(first, second)
-    print(total)
+    print()
+    print(sum_list(first, second))
+    print(sum_list_follow_up(first, second))
