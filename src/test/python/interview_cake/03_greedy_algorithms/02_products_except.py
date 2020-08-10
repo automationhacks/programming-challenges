5d187c79f729f06d881bb35173ef4b7addf276ef
Gaurav Singh <gaurav.singh55@gmail.com>
2020-08-10 07:50:53 +0530

    Product except current no problem solution

---
+++ src/test/python/interview_cake/03_greedy_algorithms/02_products_except.py
@@ -0,0 +1,103 @@
+import unittest
+
+"""
+You have a list of integers, and for each index you want to find the product of every integer except the integer 
+at that index.
+
+Write a function get_products_of_all_ints_except_at_index() that takes a list of integers and returns 
+a list of the products.
+
+For example, given:
+
+  [1, 7, 3, 4]
+
+your function would return:
+
+  [84, 12, 28, 21]
+
+by calculating:
+
+  [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+
+Here's the catch: You can't use division in your solution! 
+"""
+
+"""
+Solution: O(n) time and space
+
+ To find the products of all the integers except the integer at each index, we'll go through our list greedily ↴ twice. 
+ First we get the products of all the integers before each index, and then we go backwards to get the products of all 
+ the integers after each index.
+
+When we multiply all the products before and after each index, we get our answer—the products of all the integers 
+except the integer at each index! 
+"""
+
+
+def get_products_of_all_ints_except_at_index(int_list):
+    # edge case when list is empty or has less than 2 numbers
+    if len(int_list) < 2:
+        raise ValueError("List should not be emptry or less than 2 items")
+
+    products_of_all_ints_except_at_index = [None] * len(int_list)
+
+    # For each integer, we find the product of all the integers
+    # before it, storing the total product so far each time
+    product_so_far = 1
+    for i in range(len(int_list)):
+        products_of_all_ints_except_at_index[i] = product_so_far
+        product_so_far *= int_list[i]
+
+    # For each integer, we find the product of all the integers
+    # after it. since each index in products already has the
+    # product of all the integers before it, now we're storing
+    # the total product of all other integers
+    product_so_far = 1
+    for i in range(len(int_list) - 1, -1, -1):
+        products_of_all_ints_except_at_index[i] *= product_so_far
+        product_so_far *= int_list[i]
+
+    return products_of_all_ints_except_at_index
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_small_list(self):
+        actual = get_products_of_all_ints_except_at_index([1, 2, 3])
+        expected = [6, 3, 2]
+        self.assertEqual(actual, expected)
+
+    def test_longer_list(self):
+        actual = get_products_of_all_ints_except_at_index([8, 2, 4, 3, 1, 5])
+        expected = [120, 480, 240, 320, 960, 192]
+        self.assertEqual(actual, expected)
+
+    def test_list_has_one_zero(self):
+        actual = get_products_of_all_ints_except_at_index([6, 2, 0, 3])
+        expected = [0, 0, 36, 0]
+        self.assertEqual(actual, expected)
+
+    def test_list_has_two_zeros(self):
+        actual = get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0])
+        expected = [0, 0, 0, 0, 0]
+        self.assertEqual(actual, expected)
+
+    def test_one_negative_number(self):
+        actual = get_products_of_all_ints_except_at_index([-3, 8, 4])
+        expected = [32, -12, -24]
+        self.assertEqual(actual, expected)
+
+    def test_all_negative_numbers(self):
+        actual = get_products_of_all_ints_except_at_index([-7, -1, -4, -2])
+        expected = [-8, -56, -14, -28]
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_list(self):
+        with self.assertRaises(Exception):
+            get_products_of_all_ints_except_at_index([])
+
+    def test_error_with_one_number(self):
+        with self.assertRaises(Exception):
+            get_products_of_all_ints_except_at_index([1])
